2a6188c914857223c8429f005027876daa5644f3
qidaink <2038035593@qq.com>
2022-05-09 16:35:41 +0800

    添加诗词标签.

--- index.js
+++ index.js
@@ -272,3 +272,15 @@ function postCell(args, content) {
 hexo.extend.tag.register('btns', postBtns, {ends: true});
 hexo.extend.tag.register('cell', postCell);
 
+/** --------------------------------------------------------- */
+/** 诗词 poem
+ * poem.js
+ */
+function poem (args, content) {
+  args = args.join(' ').split(',')
+  let p0 = args[0]
+  let p1 = args[1]?args[1]:''
+  return `<div class='poem'><div class='poem-title'>${p0}</div><div class='poem-author'>${p1}</div>${hexo.render.renderSync({ text: content, engine: 'markdown' })}</div>`
+}
+
+hexo.extend.tag.register('poem',poem,{ ends: true });
